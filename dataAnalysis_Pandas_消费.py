1ae34d0dab356c451a22e3a57cfedb5784627c2b
once_fly <36881458+ArronVout@users.noreply.github.com>
2020-12-16 23:48:53 +0800

    Update dataAnalysis_Pandas_消费.py

--- dataAnalysis_Pandas_消费.py
+++ dataAnalysis_Pandas_消费.py
@@ -14,7 +14,7 @@ OUT_FILE_NAME = "会员卡消费清单_ALL.xlsx"
 HOTEL_LIST=["集团本部","长江半岛酒店","湄潭酒店","土城圣地客栈","遵义宾馆","凤凰酒店","侏罗纪酒店","大瀑布酒店","机场酒店","鑫达鑫酒店","星火酒店","余庆健康酒店"]
 COL = ["酒店","卡号","客户姓名","消费金额","前台房号","前台账号","餐饮单号","操作时间","操作员"]
 TODAY_FILE = "会员卡充值清单_"+time.strftime("%Y-%m-%d",time.localtime())+".xlsx"  #生成当天的文件
-JIUCOL = ["卡号","客户姓名","消费金额","操作时间"]
+JIUCOL = ["消费酒店","卡号","客户姓名","消费金额","操作时间"]
 CONFIG = "config.xlsx"
 XF = "酒类卡消费清单.xls"
 ##-------config info  end------
@@ -74,25 +74,28 @@ NewXF = []#pd.DataFrame([],)
 for i in range(0,df.shape[0]):   #df.shape[0]: 行的数量，1：列的数量
     tpstr = str(df.iloc[i,0])
     cardstr =str(df.iloc[i,1])[-8:]  # 卡号，部分是酒店首行的，则可能是消费数据的部分内容
-    print(cardstr)
+##    print(cardstr)
     if cardstr in str(CardInfo):  # CardIfo 为Int64, 而cardstr 为字符串。
-        NewXF.append(list(df.iloc[i,[0,1,2,6]]))
+        temp1 = list(df.iloc[i,[0,1,2,5,9]])
+        temp1[1] = cardstr
+        NewXF.append(temp1)        
     if TestStr in str(tpstr):    #包含有"酒店名称"，则说明是某家的统计起始行
         postion.append(i)
         hotel.append(tpstr[5:])
 
-print("----酒卡的消费-----")
-print(NewXF)
+##print("----酒卡的消费-----")
+##print(NewXF)
 NX2 = pd.DataFrame(NewXF,columns=JIUCOL)
+NX2 = NX2.sort_values(by = "操作时间")
 NX2.to_excel(writer_XF,index=False)
 writer_XF.save()
 
 postion.append(df.shape[0])     # 将最后一行的位置，加入位置清单，供备用
 hotel.append("this is the end")  # 最后一行对应的名字
 
-print("----------------打印：各酒店开头行的所在位置及对应的酒店-----------------------")
-print(postion)
-print(hotel)
+##print("----------------打印：各酒店开头行的所在位置及对应的酒店-----------------------")
+##print(postion)
+##print(hotel)
 
 postionCount = len(postion)     #确认当前列表数据个数，即：有充值的酒店数量+1
 ##print("列表个数：:" + str(postionCount))
@@ -106,22 +109,22 @@ for j in range(0, postionCount-1):
 
 ##以已提取的数据为基础，按标准格式创建空表帧 
 GetedInfo = pd.DataFrame(rowdata,columns=COL)
-print(GetedInfo)
+##print(GetedInfo)
 
 if postionCount > 1:  ## 有数据，则创建对应数据的充asdfasdfasdf值报表文件
 ##    DataFileName = "会员卡充值清单_"+time.strftime("%Y-%y-%d",time.localtime())+".xlsx"  #生成当天的文件
     DataFileName = "会员卡消费清单_"+str(df.iloc[1,9])[0:10]+".xlsx"  #生成当天的文件
-    print(DataFileName)
+##    print(DataFileName)
     DataFile = pd.DataFrame([],columns=COL)
     DataFile.to_excel(DataFileName,index=False)
-    print("创建当天的文件：" + DataFileName)
+##    print("创建当天的文件：" + DataFileName)
     writer_DataFile = pd.ExcelWriter(DataFileName)
 else:
     print("无消费数据")
     exit()
 
     
-print("----------------打印：各酒店开头行的所在位置-----------------------")
+##print("----------------打印：各酒店开头行的所在位置-----------------------")
 
 #建立输出引擎，将所有数据，归总到各酒店，并将统计日期的数据，单独形成报表
 writer = pd.ExcelWriter(OUT_FILE_NAME)
@@ -137,16 +140,16 @@ for hotelname in HOTEL_LIST:
         print("----------------打印：输出文件不存在，新建一个-----------------------")
         PreInfo = pd.DataFrame([],columns = COL)
         
-    print("----------------打印：当前酒店："+hotelname+"-----------------------")   
+##    print("----------------打印：当前酒店："+hotelname+"-----------------------")   
     if hotelname in hotel: #如果当前便利的酒店名字在充值酒店清单中，则有新数据需要合并，没有则保持原数据
         NewInfo = GetedInfo.loc[GetedInfo["酒店"] == hotelname,]  #按酒店名称，将所有充值数据提取；
         PreInfo = PreInfo.append(NewInfo)   #数据帧对象的append 不是本地修改，而是创建副本，因此重新赋值
-        print("新消费信息----->")
-        print(NewInfo)
+##        print("新消费信息----->")
+##        print(NewInfo)
         try:
             Input = pd.read_excel(writer_DataFile,sheet_name = hotelname)  #获取hotelname酒店对应的前期数据
         except:
-            print("meiyou sheet")
+            print("没有 sheet，需要创建,当前酒店：" + hotelname)
             Input = pd.DataFrame([],columns = COL)
 
 ##        NewInfo = np.abs(NewInfo) 当前是Series类型
@@ -155,17 +158,18 @@ for hotelname in HOTEL_LIST:
 ##        NewInfo = NewInfo.add(pd.Series("","","",NewInfo["充值金额"].cumsum(),"",""))
         
         Input = Input.append(NewInfo)
-                    
-        print("添加后,增加求和的----->Input")
-        print(Input)
+##        print("添加后,增加求和的----->Input")
+##        print(Input)
+        Input = Input.sort_values(by = "操作时间")
         Input.to_excel(writer_DataFile,sheet_name = hotelname,index=False)  #将统计日期的充值数据写入当天的报表
         
-    print("----------------整理后的数据信息添加后的:去重复前----->PreInfo-----------------------")
-    print(PreInfo)    
+##    print("----------------整理后的数据信息添加后的:去重复前----->PreInfo-----------------------")
+##    print(PreInfo)    
     #hotelname 酒店的数据合并处理完成，导出
     PreInfo = PreInfo.drop_duplicates("操作时间")   #去除重复项，防止对某天进行多次执行的情况
-    print("----------------去重复后--------------------------")
-    print(PreInfo)    
+    PreInfo = PreInfo.sort_values(by = "操作时间")
+##    print("----------------去重复后--------------------------")
+##    print(PreInfo)    
     PreInfo.to_excel(writer,sheet_name = hotelname,index=False)
     
 GetedInfo.to_excel(writer,OUT_FILE_NAME)
